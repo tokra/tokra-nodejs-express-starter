f04ca41ca36cd11a41305b0b16085935c53ac15b
to-kra <code@tokra.io>
2022-05-17 09:44:15 +0200

    prettier

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 // if production, enable helmet
 /* c8 ignore next 3  */
 if (process.env.VCAP_APPLICATION) {
-  app.use(helmet());
+	app.use(helmet());
 }
 
 // enable parsing of http request body
@@ -30,19 +30,19 @@ app.use('/swagger', swaggerRoutes);
 
 // default path to serve up index.html (single page application)
 app.all('', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../public', 'index.html'));
+	res.status(200).sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // start node server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App UI available http://localhost:${port}`);
-  console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
+	console.log(`App UI available http://localhost:${port}`);
+	console.log(`Swagger UI available http://localhost:${port}/swagger/api-docs`);
 });
 
 // error handler for unmatched routes or api calls
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, '../public', '404.html'));
+	res.sendFile(path.join(__dirname, '../public', '404.html'));
 });
 
 export default app;
